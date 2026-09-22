2aadb9805b21c2b4123b55bca176c2f492297a77
CheFu-code <kurisanimaluleke77@gmail.com>
2026-09-22 10:48:48 +0200

    Update project name in package.json and metadata in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const poppins = Poppins({
 })
 
 export const metadata: Metadata = {
-  title: "Cloudence",
+  title: "Cloudence | Chefu Technologies",
   description: "Cloudence - Beyond storage, into the clouds.",
 };
 
